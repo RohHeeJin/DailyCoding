fedc6073f92bb4c7cd2c7437370d6647e07f16a2
RohHeeJin <ranin00421@gmail.com>
2022-06-28 23:49:30 +0900

    10.isSubsetOf

---
+++ 10.isSubsetOf.js
@@ -0,0 +1,45 @@
+// 문제 : 두 개의 배열(base, sample)을 입력받아 sample이 base의 부분집합인지 여부를 리턴해야 합니다.
+// input : number 타입을 요소로 갖는 임의의 배열
+//         base.length는 100 이하
+
+// output : number 타입을 요소로 갖는 임의의 배열
+//          sample.length는 100 이하
+
+const isSubsetOf = function (base, sample) {
+  // naive solution: O(M * N)
+  // return sample.every((item) => base.includes(item));
+
+  // 각 배열을 정렬: O(N * logN), O(M * logM)
+  // N >= M 이므로, O(N * logN)
+  base.sort((a, b) => a - b);
+  sample.sort((a, b) => a - b);
+
+  const findItemInSortedArr = (item, arr, from) => {
+    for (let i = from; i < arr.length; i++) {
+      if (item === arr[i]) return i;
+      else if (item < arr[i]) return -1;
+    }
+    return -1;
+  };
+
+  let baseIdx = 0;
+  for (let i = 0; i < sample.length; i++) {
+    baseIdx = findItemInSortedArr(sample[i], base, baseIdx);
+    if (baseIdx === -1) return false;
+  }
+  return true;
+};
+
+let base = [1, 2, 3, 4, 5];
+let sample = [1, 3];
+let output = isSubsetOf(base, sample);
+console.log(output); // --> true
+
+sample = [6, 7];
+output = isSubsetOf(base, sample);
+console.log(output); // --> false
+
+base = [10, 99, 123, 7];
+sample = [11, 100, 99, 123];
+output = isSubsetOf(base, sample);
+console.log(output); // --> false
